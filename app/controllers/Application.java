98b9eaf999957a3597ae7b049a4e0bd719036328
virtualcrow <virtualcrow@virtualcrow.(none)>
2013-05-27 13:40:52 +0800

    added testcase that uses neo4j embedded

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,7 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import play.mvc.Controller;
@@ -12,7 +13,7 @@ import views.html.index;
 
 public class Application extends Controller {
 
-	private static final String NEO4J_DB_PATH = "/home/virtualcrow/stack/neo4j/data/graph.db";
+	private static final String NEO4J_DB_PATH = "/home/virtualcrow/stack/neo4j/data/graph3.db";
 
 	private static enum RelTypes implements RelationshipType {
 		NextEvent, PreviousEvent
@@ -25,20 +26,40 @@ public class Application extends Controller {
 	public static Result cypher() {
 		GraphDatabaseService graphDb = new GraphDatabaseFactory()
 				.newEmbeddedDatabase(NEO4J_DB_PATH);
+		registerShutdownHook(graphDb);
 
-		Node firstEvent = graphDb.createNode();
-		firstEvent.setProperty("id", "12345");
-		firstEvent.setProperty("type", "testnode");
+		Transaction tx = graphDb.beginTx();
+		try {
+			Node firstEvent = graphDb.createNode();
+			firstEvent.setProperty("id", "12345");
+			firstEvent.setProperty("type", "testnode");
 
-		Node secondEvent = graphDb.createNode();
-		secondEvent.setProperty("id", "12345678");
+			Node secondEvent = graphDb.createNode();
+			secondEvent.setProperty("id", "12345678");
 
-		Relationship goRelationship = firstEvent.createRelationshipTo(
-				secondEvent, RelTypes.NextEvent);
+			Relationship goRelationship = firstEvent.createRelationshipTo(
+					secondEvent, RelTypes.NextEvent);
+
+			tx.success();
+		} finally {
+			tx.finish();
+		}
 
 		graphDb.shutdown();
 
 		return ok(index.render("Cypher stuff has been happening!!!"));
+	}
+
+	private static void registerShutdownHook(final GraphDatabaseService graphDb) {
+		// Registers a shutdown hook for the Neo4j instance so that it
+		// shuts down nicely when the VM exits (even if you "Ctrl-C" the
+		// running example before it's completed)
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				graphDb.shutdown();
+			}
+		});
 	}
 
 }
